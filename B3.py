78b83089e36c33f4be8b9cde05902f838cf9a238
opilane <opilane@HP-CZC2349HT9>
2023-02-20 14:49:46 +0200

    Add project files.

---
+++ B3.py
@@ -0,0 +1,29 @@
+﻿from moodul import *
+
+print("Arkadi Korotitsh TARpv22 poolt\n")
+
+linnad=[]
+elanikkond=[]
+
+n=int(input("Sisestage linna arv: "))
+
+for j in range(n):
+    linn=input("Sisestage linna nimi: ")
+    elanik=int(input("Sisestage selle linna elanikkond: "))
+    linnad.append(linn)
+    elanikkond.append(elanik)
+
+print(linnad,elanikkond)
+
+while True:
+        menu=int(input("Käsklused: \n1-saab teada, kui palju on elanikke, sisestades linna nime\n2- Kuvab linnade nimekirja ja elanike arvu tähestikulises järjekorras\n3-saab linna rahvaarvu, sisestades linna nime\n4-saab linnad, kus on vähem kui n elanikku\n5-saab suurima rahvaarvuga linna\n"))
+        if menu==1:
+            get_population(elanikkond,linnad)
+        elif menu==2:
+            show_inf(elanikkond,linnad)
+        elif menu==3:
+            get_city(elanikkond,linnad)
+        elif menu==4:
+            less(elanikkond,linnad)
+        elif menu==5:
+            most_pop(elanikkond,linnad)
---
+++ moodul.py
@@ -0,0 +1,42 @@
+﻿def get_population(e:list,l:list):
+    linn=input("Sisestage linna nimi, mille elanike arvu soovite teada:")
+    while linn not in l:
+        linn=input("Palun kirjuta õige linna nimi\n")
+    print(f"{linn} rahvaarv on võrdne ",e[l.index(linn)],"")
+
+def get_city(e:list,l:list):
+    arv=int(input("Sisestage elanike arv: "))
+    erinev_ls=[]
+    while arv<1:
+        arv=int(input("Arv peab olema suurem kui 0: "))
+    for i in e:
+        erinevus=abs(i-arv)
+        erinev_ls.append(erinevus)
+    min_erinevus=min(erinev_ls)
+    print("Selle linna ligikaudne rahvaarv: ",l[erinev_ls.index(min_erinevus)])
+    
+def less(e:list,l:list):
+    n=int(input("Sisestage elanike arv: "))
+    linna=[]
+    while n<1:
+        n=int(input("Arv peab olema suurem kui 0: "))
+    for i in e:
+        if n>i:
+            elanik_index=e.index(i)
+            linna.append(l[elanik_index])
+    print("Nendes linnades on elanike arv väiksem: ",linna)
+
+def show_inf(e:list,l:list):
+    l_copy=l.copy()
+    l_copy.sort()
+    sorted_elanikud=[]
+    for element in l_copy:
+        temp_var=l.index(element)
+        sorted_elanikud.append(e[temp_var])
+    print(l_copy)
+    print(sorted_elanikud)
+    print()
+
+def most_pop(e:list,l:list):
+    print("Suurima elanikkonnaga linn: ")
+    print(min(e), l[e.index(min(e))])
